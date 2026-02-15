9b20585738ff5c242aeced29d5f442eaefedc933
Israthaneem <israthaneem@gmail.com>
2026-02-16 02:11:45 +0530

    resolved stash conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import Contact from './Contact';
 function App() {
-  return <Contact />;
+  return (
+    <div className="h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">Tailwind is working!</h1>
+    </div>
+  );
 }
 
 export default App;
